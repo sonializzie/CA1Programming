5e058713e547e2f65246607b77a192578c6b19ae
Sonia Ndonga <ndongasonia@gmail.com>
2024-02-03 23:02:43 +0000

    Initial commit

---
+++ src/ca1programming/CA1Programming.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ca1programming;
+
+/**
+ *
+ * @author sonia_ndonga
+ * 
+ * Module : Object Oriented Programming : CA1
+ * Date : 3rd February 2024
+ * 
+ */
+public class CA1Programming {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
