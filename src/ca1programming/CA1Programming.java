3f1699c4515f090511c2b9641b4ce8a492c926a4
Sonia Ndonga <ndongasonia@gmail.com>
2024-02-04 01:53:10 +0000

    Created a processStudentFileData() method
    - Created a private method for processStudentFileData()
    
    - Created BufferedReader w/h FileReader "student.txt"
    
    - Created BufferedWriter w/h FileWriter "status.txt"
    
    - Added a while loop for BufferedReader
    
    - If else statement for BufferedWriter

--- src/ca1programming/CA1Programming.java
+++ src/ca1programming/CA1Programming.java
@@ -4,6 +4,11 @@
  */
 package ca1programming;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 /**
  *
  * @author sonia_ndonga
@@ -25,5 +30,52 @@ public class CA1Programming {
         
         
     }//End of main method
+    
+    
+    private static void processStudentFileData(){
+        // Beginning of processStudentFileData
+        
+        //try catch block
+        //Creates a BufferedReader using FileReader with "students.txt"
+        //Creates a BufferedWriter using FileWriter with "status.txt"
+        
+        try(BufferedReader br = new BufferedReader(new FileReader("students.txt")); 
+            BufferedWriter bw = new BufferedWriter(new FileWriter("status.txt"));){
+            
+            //Inputting variable for line
+            String line;
+            
+            //Beginning of while loop
+            //Reading line from BufferedReader
+            while((line = br.readLine()) != null){
+                //Reads characters from BufferedReader
+                String firstName = line;
+                String lastName = br.readLine();
+                int numberOfClasses = Integer.parseInt(br.readLine());
+                String studentNumber = br.readLine();
+                
+                //Declaring variable for 'Student' of type 's' 
+                //The parameters are passed through the constructor from the Student.java class
+                Student s = new Student(firstName, lastName, numberOfClasses, studentNumber);
+                
+                //If else statement for BufferedWriter
+                if(s.isValid()) { 
+                    bw.write(s.getFormattedOutput()); // Insert write for getFormattedOutput()
+                    bw.newLine(); //Insert newLine for BufferedWriter
+                    bw.newLine();
+                }else{
+                    System.out.println("Invalid Student Data:" + studentNumber);
+                }
+                          
+            }
+            
+            System.out.println("Student Data is processed successfully.");
+            
+            
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        
+    }// End of processStudentFileData
     
 }//End of class method 
