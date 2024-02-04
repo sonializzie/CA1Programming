c34235976148cf43932fc2ef50640e71e1cb133a
Sonia Ndonga <ndongasonia@gmail.com>
2024-02-04 00:38:56 +0000

    Create a method for isValid()
    Validated the class attributes for Student.

--- src/ca1programming/Student.java
+++ src/ca1programming/Student.java
@@ -29,6 +29,42 @@ public class Student {
         this.studentNumber = studentNumber;
     }
     
+    public boolean isValid(){
+        // Beggining of isValid method
+        
+        // Validating firstName
+        if(!firstName.matches("[a-zA-Z]+")){
+            return false;
+        }
+        
+        // Validating lastName
+        if(!lastName.matches("a-zA-Z0-9+")){
+            return false;
+        }
+        
+        // Validating numberOfClasses
+        if(numberOfClasses < 1 || numberOfClasses > 8){
+            return false;
+        }
+        
+        // Validating studentNumber
+        if(studentNumber.matches("\\d{2}[a-zA-Z]{1,2}\\d+")){
+            return false;
+        }
+        
+        //Inputting variables for studentNumber
+        int year = Integer.parseInt(studentNumber.substring(0,2));
+        int numberAfterLetters = Integer.parseInt(studentNumber.substring(2, studentNumber.length() - 1));
+        
+        // Validate the student number year and number after letters
+        if (year < 20 || numberAfterLetters < 1 || numberAfterLetters > 200){
+            return false;
+        }
+        
+        //return true is for isValid()
+        return true;
+    }// End of isValid method
+    
     
     
     
