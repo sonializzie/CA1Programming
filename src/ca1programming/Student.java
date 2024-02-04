f01e40446255ad539f8bbe167d1e49dc9e7c589f
Sonia Ndonga <ndongasonia@gmail.com>
2024-02-04 00:13:26 +0000

    Created Class Attribute and Constructor
    Class Attributes for Student
    
    - String firstName
    - String lastName
    - int numberOfClasses
    - String studentNumber

--- src/ca1programming/Student.java
+++ src/ca1programming/Student.java
@@ -9,5 +9,27 @@ package ca1programming;
  * @author sonia_ndonga
  */
 public class Student {
+    //Beginning class for Student
+    //Created a class attribute for Student 
     
-}
+    private String firstName;
+    private String lastName;
+    private int numberOfClasses;
+    private String studentNumber;
+    
+    public Student(){
+        //Created a Student Method
+    }
+
+    //Created a class constructor for Student 
+    public Student(String firstName, String lastName, int numberOfClasses, String studentNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.numberOfClasses = numberOfClasses;
+        this.studentNumber = studentNumber;
+    }
+    
+    
+    
+    
+}//End of student class
